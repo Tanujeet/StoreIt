b0accdf7ffc1ab59dbeae2dc60a142a32642129c
Tanujeet <singhtanujeet@gmail.com>
2025-06-16 20:14:18 +0530

    implemented sign in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,11 @@
+import AuthForm from "@/components/AuthForm";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <AuthForm type="sign-in" />
+    </div>
+  );
 };
 
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,11 @@
+import AuthForm from "@/components/AuthForm";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <AuthForm type="sign-up" />
+    </div>
+  );
 };
 
 export default SignUp;
