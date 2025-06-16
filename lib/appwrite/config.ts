a7f9d9efc884c868f7ffa0ea65595a29cde8bbb2
Tanujeet <singhtanujeet@gmail.com>
2025-06-16 21:07:17 +0530

    implemented appwrite env

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+  endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+  projectID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!,
+  databaseID: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+  usersCollectionID: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+  filesCollectionID: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
+  bucketID: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+  secretKey: process.env.NEXT_PUBLIC_APPWRITE_KEY!,
+};
