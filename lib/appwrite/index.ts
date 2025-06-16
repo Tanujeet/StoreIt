96205d529d465019b99b3d66696f99c6d7d01004
Tanujeet <singhtanujeet@gmail.com>
2025-06-16 21:12:17 +0530

    implemented appwrite env

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,9 @@
+import { Client } from 'node-appwrite'
+import { appwriteConfig } from './config'
+
+
+export const createSessionClient = async () => {
+    const client = new Client()
+        .setEndpoint(appwriteConfig.endpointUrl)
+        .setProject(appwriteConfig.projectID)
+}
