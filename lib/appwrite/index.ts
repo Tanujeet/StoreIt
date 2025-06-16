625a00223ffa8160beaf7740c44da80b96a75e1e
Tanujeet <singhtanujeet@gmail.com>
2025-06-16 21:18:59 +0530

    implemented appwrite index.ts

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,4 +1,4 @@
-import { Account, Client, Databases } from "node-appwrite";
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
 import { appwriteConfig } from "./config";
 import { cookies } from "next/headers";
 
@@ -21,4 +21,24 @@ export const createSessionClient = async () => {
   };
 };
 
-export const createAdminClient = async () => {};
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectID)
+    .setKey(appwriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get database() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatar() {
+      return new Avatars(client);
+    },
+  };
+};
