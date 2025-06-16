9d4893c1628c7c9b3fce97e6fcaa5c3b90f3bf9c
Tanujeet <singhtanujeet@gmail.com>
2025-06-16 21:16:44 +0530

    implemented appwrite index.ts

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,9 +1,24 @@
-import { Client } from 'node-appwrite'
-import { appwriteConfig } from './config'
-
+import { Account, Client, Databases } from "node-appwrite";
+import { appwriteConfig } from "./config";
+import { cookies } from "next/headers";
 
 export const createSessionClient = async () => {
-    const client = new Client()
-        .setEndpoint(appwriteConfig.endpointUrl)
-        .setProject(appwriteConfig.projectID)
-}
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectID);
+
+  const session = (await cookies()).get("appwrite-session");
+  if (!session || !session.value) throw new Error("No Session");
+
+  client.setSession(session.value);
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get database() {
+      return new Databases(client);
+    },
+  };
+};
+
+export const createAdminClient = async () => {};
