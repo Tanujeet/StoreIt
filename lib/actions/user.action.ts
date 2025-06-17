c2b9db09ddf301b75f444d13b58b81858a63f7ba
Tanujeet <singhtanujeet@gmail.com>
2025-06-17 07:43:12 +0530

    implemented user.action.ts

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { Query } from "node-appwrite";
+import { Query, ID } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 
@@ -11,6 +11,23 @@ const getUserbyEmail = async (email: string) => {
     appwriteConfig.usersCollectionID,
     [Query.equal("email", [email])]
   );
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send OTP");
+  }
 };
 
 const createAccont = async ({
@@ -21,4 +38,5 @@ const createAccont = async ({
   email: string;
 }) => {
   const existingUser = await getUserbyEmail(email);
+  const accountId = await sendEmailOTP({ email });
 };
