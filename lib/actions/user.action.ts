d33323e195556c9be20d30d678b0c0eba6d1b9b2
Tanujeet <singhtanujeet@gmail.com>
2025-06-17 08:10:41 +0530

    changes in user.actions.ts

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,19 +1,22 @@
 "use server";
 
+import { createAdminClient, createSessionClient } from "@/lib/appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
 import { Query, ID } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
-import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "@/lib/utils";
+import { cookies } from "next/headers";
 import { avatarPlaceholderUrl } from "@/constants";
-import { parse } from "path";
-import { parseStringify } from "../utils";
+import { redirect } from "next/navigation";
 
-const getUserbyEmail = async (email: string) => {
+const getUserByEmail = async (email: string) => {
   const { database } = await createAdminClient();
+
   const result = await database.listDocuments(
     appwriteConfig.databaseID,
     appwriteConfig.usersCollectionID,
     [Query.equal("email", [email])]
   );
+
   return result.total > 0 ? result.documents[0] : null;
 };
 
@@ -22,30 +25,33 @@ const handleError = (error: unknown, message: string) => {
   throw error;
 };
 
-const sendEmailOTP = async ({ email }: { email: string }) => {
+export const sendEmailOTP = async ({ email }: { email: string }) => {
   const { account } = await createAdminClient();
 
   try {
     const session = await account.createEmailToken(ID.unique(), email);
+
     return session.userId;
   } catch (error) {
-    handleError(error, "Failed to send OTP");
+    handleError(error, "Failed to send email OTP");
   }
 };
 
-export const createAccont = async ({
+export const createAccount = async ({
   fullName,
   email,
 }: {
   fullName: string;
   email: string;
 }) => {
-  const existingUser = await getUserbyEmail(email);
-  const accountId = await sendEmailOTP({ email });
+  const existingUser = await getUserByEmail(email);
 
+  const accountId = await sendEmailOTP({ email });
   if (!accountId) throw new Error("Failed to send an OTP");
+
   if (!existingUser) {
     const { database } = await createAdminClient();
+
     await database.createDocument(
       appwriteConfig.databaseID,
       appwriteConfig.usersCollectionID,
@@ -58,5 +64,80 @@ export const createAccont = async ({
       }
     );
   }
+
   return parseStringify({ accountId });
+};
+
+export const verifySecret = async ({
+  accountId,
+  password,
+}: {
+  accountId: string;
+  password: string;
+}) => {
+  try {
+    const { account } = await createAdminClient();
+
+    const session = await account.createSession(accountId, password);
+
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify({ sessionId: session.$id });
+  } catch (error) {
+    handleError(error, "Failed to verify OTP");
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const { database, account } = await createSessionClient();
+
+    const result = await account.get();
+
+    const user = await database.listDocuments(
+      appwriteConfig.databaseID,
+      appwriteConfig.usersCollectionID,
+      [Query.equal("accountId", result.$id)]
+    );
+
+    if (user.total <= 0) return null;
+
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signOutUser = async () => {
+  const { account } = await createSessionClient();
+
+  try {
+    await account.deleteSession("current");
+    (await cookies()).delete("appwrite-session");
+  } catch (error) {
+    handleError(error, "Failed to sign out user");
+  } finally {
+    redirect("/sign-in");
+  }
+};
+
+export const signInUser = async ({ email }: { email: string }) => {
+  try {
+    const existingUser = await getUserByEmail(email);
+
+    // User exists, send OTP
+    if (existingUser) {
+      await sendEmailOTP({ email });
+      return parseStringify({ accountId: existingUser.accountId });
+    }
+
+    return parseStringify({ accountId: null, error: "User not found" });
+  } catch (error) {
+    handleError(error, "Failed to sign in user");
+  }
 };
