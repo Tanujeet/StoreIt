15fb7bdb5b92b565abd49e9b9be31635b01354d1
Tanujeet <singhtanujeet@gmail.com>
2025-06-18 07:47:36 +0530

    implemented header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      Search
+      <div className="header-wrapper">
+        File uploader
+        <form>
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
