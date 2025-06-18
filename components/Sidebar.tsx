1691c9f70a98e6dc62ffa7e128b190787c32b798
Tanujeet <singhtanujeet@gmail.com>
2025-06-18 07:54:06 +0530

    implemented sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,13 +6,23 @@ const Sidebar = () => {
     <aside className="sidebar">
       <Link href="/">
         <Image
-          src="/assets/icons/logo-full.svg"
+          src="/assets/icons/logo-full-brand.svg"
           alt="logo"
           width={160}
           height={50}
           className="hidden h-auto lg:block"
         />
+        <Image
+          src="/assets/icons/logo-brand.svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
       </Link>
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6"></ul>
+      </nav>
     </aside>
   );
 };
