5d2b02afd031e48b3d36fb9d2587e13d3f5bc4fe
Tanujeet <singhtanujeet@gmail.com>
2025-06-18 07:51:51 +0530

    implemented sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="sidebar">
+      <Link href="/">
+        <Image
+          src="/assets/icons/logo-full.svg"
+          alt="logo"
+          width={160}
+          height={50}
+          className="hidden h-auto lg:block"
+        />
+      </Link>
+    </aside>
+  );
 };
 
 export default Sidebar;
