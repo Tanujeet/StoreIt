83f2fbf297a471890d05059494ba4499a6d8d06a
Tanujeet <singhtanujeet@gmail.com>
2025-06-19 10:09:06 +0530

    implemented mobile navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,7 @@ const layout = async ({ children }: { children: React.ReactNode }) => {
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
         <MobileNavigation />
-        <Header />
+        <Header {...currentUser} />
         <div className="main-content">{children}</div>
       </section>
     </main>
--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,5 +1,73 @@
-const MobileNavigation = () => {
-  return <div>MobileNavigation</div>;
+"use client";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+interface Props {
+  fullname: string;
+  avatar: string;
+  email: string;
+  ownerId: string;
+  accountId: string;
+}
+const MobileNavigation = ({
+  fullname,
+  avatar,
+  email,
+  ownerId,
+  accountId,
+}: Props) => {
+  const [open, setOpen] = useState();
+  const pathname = usePathname();
+  return (
+    <header className="mobile-header">
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="logo"
+        width={120}
+        height={52}
+        className="h-auto"
+      />
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="Search"
+            width={30}
+            height={30}
+          />
+        </SheetTrigger>
+        <SheetContent className="shad-sheet h-screen px-3">
+          <SheetTitle>
+            <div className="header-user">
+              <Image
+                src={avatar}
+                alt="avatar"
+                height={44}
+                width={44}
+                className="header-user-avatar"
+              />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullname}</p>
+              </div>
+            </div>
+          </SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 };
 
 export default MobileNavigation;
