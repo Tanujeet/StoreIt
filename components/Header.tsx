28142845fe62d811e9179093745e3218afa19fce
Tanujeet <singhtanujeet@gmail.com>
2025-06-20 10:26:08 +0530

    implemented file action

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,26 @@
+import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Search from "./Search";
-import FileUploader from "./FileUploader";
+import Search from "@/components/Search";
+import FileUploader from "@/components/FileUploader";
 import { signOutUser } from "@/lib/actions/user.action";
-import { useState } from "react";
-import { getFileType } from "@/lib/utils";
 
-const Header = () => {
-  const [files, setFiles] = useState<File[]>([]);
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
   return (
     <header className="header">
       <Search />
       <div className="header-wrapper">
-        <FileUploader />
+        <FileUploader ownerId={userId} accountId={accountId} />
         <form
           action={async () => {
             "use server";
+
             await signOutUser();
           }}
         >
@@ -27,20 +32,10 @@ const Header = () => {
               height={24}
               className="w-6"
             />
-            <p>Upload</p>
           </Button>
-          {files.length > 0 && (
-            <ul className="uploader-preview-list">
-              <h4 className="h4 text-light-100">Uploading</h4>
-              {files.map((files, index) => {
-                const { type, extension } = getFileType(files.name);
-              })}
-            </ul>
-          )}
         </form>
       </div>
     </header>
   );
 };
-
 export default Header;
