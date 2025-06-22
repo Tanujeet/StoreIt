f46c53d602088f268133e22e7a006ad14c35f918
Tanujeet <singhtanujeet@gmail.com>
2025-06-22 20:45:46 +0530

    Added : changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,23 +1,30 @@
-import Header from "@/components/Header";
-import MobileNavigation from "@/components/MobileNavigation";
+import React from "react";
 import Sidebar from "@/components/Sidebar";
-import { getCurrentUser } from "@/lib/actions/user.action";
+import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
 import { redirect } from "next/navigation";
+import { Toaster } from "@/components/ui/toaster";
+import { getCurrentUser } from "@/lib/actions/user.action";
 
-const layout = async ({ children }: { children: React.ReactNode }) => {
+export const dynamic = "force-dynamic";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
+
   if (!currentUser) return redirect("/sign-in");
 
   return (
     <main className="flex h-screen">
       <Sidebar {...currentUser} />
+
       <section className="flex h-full flex-1 flex-col">
         <MobileNavigation {...currentUser} />
-        <Header userId={currentUser.$id} accountId={currentUser.$accountId} />
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
       </section>
+
+      <Toaster />
     </main>
   );
 };
-
-export default layout;
+export default Layout;
