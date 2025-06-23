f3112c4639a57e4a3cf0fd2ee24125b321164205
Tanujeet <singhtanujeet@gmail.com>
2025-06-23 09:00:42 +0530

    Added:changes in actionsdropdowm

--- components/ActionsDropdown.tsx
+++ components/ActionsDropdown.tsx
@@ -1,6 +1,64 @@
-const ActionsDropdown = () => {
+"use client";
+
+import { Dialog } from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { actionsDropdownItems } from "@/constants";
+import Image from "next/image";
+import { Models } from "node-appwrite";
+import { useState } from "react";
+
+const ActionsDropdown = ({ file }: { file: Models.Document }) => {
+  const [isModelOpen, setisModelOpen] = useState(false);
+  const [isDropdownOpen, setisDropdownOpen] = useState(false);
+  const [action, setAction] = useState<ActionType | null>(null);
   return (
-    <div>ActionsDropdown</div>
+    <Dialog open={isModelOpen} onOpenChange={setisModelOpen}>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setisDropdownOpen}>
+        <DropdownMenuTrigger className="shad-no-focus">
+          <Image
+            src="/assets/icons/dots.svg"
+            alt="dots"
+            width={34}
+            height={34}
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="max-w-[200px] truncate">
+            {file.name}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {actionsDropdownItems.map((actionItem) => (
+            <DropdownMenuItem
+              key={actionItem.value}
+              className="shad-dropdown-item"
+              onClick={() => {
+                setAction(actionItem);
+
+                if (
+                  [
+                    "Rename",
+                    "Share",
+                    "Delete",
+                    "Details".includes(actionItem.value),
+                  ]
+                ) {
+                  setisModelOpen(true);
+                }
+              }}
+            >
+              {actionItem.label}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Dialog>
   );
 };
 
