7975b2fc40b190d1e1e845c34778376c1bdb7217
Tanujeet <singhtanujeet@gmail.com>
2025-06-25 10:12:08 +0530

    Added:search

--- components/Search.tsx
+++ components/Search.tsx
@@ -7,6 +7,8 @@ import { getFiles } from "@/lib/actions/file.action";
 import { Models } from "node-appwrite";
 import { useDebounce } from "use-debounce";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
 
 const Search = () => {
   const [query, setQuery] = useState("");
@@ -35,6 +37,15 @@ const Search = () => {
     }
   }, [searchQuery]);
 
+  const handleClickItem = (file: Models.Document) => {
+    setOpen(false);
+    setResults([]);
+
+    router.push(
+      `/${file.type === "video" || file.type === "audio" ? "media" : file.type + "s"}?query=${query}`
+    );
+  };
+
   return (
     <div className="search">
       <div className="search-input-wrapper">
@@ -50,6 +61,38 @@ const Search = () => {
           className="search-input"
           onChange={(e) => setQuery(e.target.value)}
         />
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  className="flex items-center justify-between"
+                  key={file.$id}
+                  onClick={() => handleClickItem(file)}
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1 text-light-200"
+                  />
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">No files found</p>
+            )}
+          </ul>
+        )}
       </div>
     </div>
   );
